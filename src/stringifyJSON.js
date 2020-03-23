cfb45deb0c44069aaccccb75182382b09239ab44
Jorge Castello <jorgio.castello@gmail.com>
2020-03-23 16:15:47 -0700

    Update logic for stringifyJSON
    1. Moved logic for stringifying numbers to the bottom of the function
    2. Added logic to stringify string types using template literals
    3. Added logic to stringify arrays
      3a. Map the array parameter using stringifyJSON on each element
      3b. join the mapped array between the opening and closing array
      brackets
      3c. return the joined array
    4. Check to see if the parameter is an object and does not equal null
      4a. Declare an empty array called objStringArray to hold the
      properties of the object after they've been stringified
      4b. Loop through the object
      4c. At each pass
        4c1. Check to see if the type of the current value is a function
        or undefined - if so, continue to the next property
        4c2. Declare a variable keyString and assign it the return value
        of stringifyJSON on the current key
        4c3. Declare a variable valueString and assign it the return value
        of stringifyJSON on the current value
        4c4. Concat keyString with a colon and the valueString, push the
        concated string into the objStringArray
      4d. Return the joined objStringArray in between the opening and
      closing object brackets

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,8 +6,6 @@
 var stringifyJSON = function(obj) {
   // your code goes here
 
-  // Make obj into string version
-  return '' + obj;
 
   // if typeof obj is a boolean
   if (typeof obj === 'boolean') {
@@ -21,20 +19,45 @@ var stringifyJSON = function(obj) {
 
   // Check typeof obj for strings
   // If true return original string with " "
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
 
   // Check if obj is an array
-  // Map obj into a stringArray
-  // return '[' + stringArray.join(,) + ']'
+  if (Array.isArray(obj)) {
+    // Map obj into a stringArray
+    var stringArray = obj.map(element => stringifyJSON(element));
+    return '[' + stringArray.join(',') + ']';
+  }
+
 
   // Check if typeof is object
-  // declare array = [];
-  // "{ 'key' : 'value'}"
-  // Loop through obj
-    // Create stringified versions of the key value pairs using stringifyJSON
-    // Declare keyString
-    // Declare valueString
-  // array.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]))
-  // return '{' + array,join(,) + '}'
+  if (typeof obj === 'object' && obj !== null) {
+    // declare array = [];
+    var objStringArray = [];
+    // Loop through obj
+    for (var key in obj) {
+      // if type of the obj is a function or is undefined
+        // continue
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      }
+      // Create stringified versions of the key value pairs using stringifyJSON
+      // Declare keyString
+      var keyString = stringifyJSON(key);
+      // Declare valueString
+      var valueString = stringifyJSON(obj[key]);
+      objStringArray.push(keyString + ':' + valueString);
+    }
 
+  // array.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]))
+    return '{' + objStringArray.join(',') + '}';
+  }
+
+  // "{ 'key' : 'value'}"
+
+
+  // Make obj into string version
+  return '' + obj;
 
 };
