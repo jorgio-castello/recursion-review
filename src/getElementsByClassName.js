71270924c01d2f530160e0a0af6963c2141544a2
Jorge Castello <jorgio.castello@gmail.com>
2020-03-23 13:28:40 -0700

    Temporary commit for current status of work

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,14 +8,24 @@ var getElementsByClassName = function(className
 ) {
   // your code here
   //declare resultArr
+  let resultArr = [];
 
   //declare inner function - getChildNodes
-  //declare a variable nodes
-  //Loop through the nodes
-  //If the node has the className,
-  //if true: push it into resultArr
-  //call the getChildNodes on each node
+  let getChildNodes = function (nodes) {
+    //Loop through the nodes
+    nodes = Array.from(nodes);
+    nodes.forEach(node => {
+      //If the node has the className,
+      if (node.classList.value === className) {
+        //if true: push it into resultArr
+        resultArr.push(node);
+      }
+      //call the getChildNodes on each node
+      return getChildNodes(node.childNodes);
+    });
+  };
 
+  getChildNodes(document);
   //return resultArr
-
+  return resultArr;
 };
