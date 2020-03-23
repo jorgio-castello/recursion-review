a61d228c8c5d0c607ca4c666e8c58c37b896f831
Jorge Castello <jorgio.castello@gmail.com>
2020-03-23 15:50:21 -0700

    Add pseudocode for stringifyJSON
    1. Implemented actual logic for booleans / number types

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,36 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  // Make obj into string version
+  return '' + obj;
+
+  // if typeof obj is a boolean
+  if (typeof obj === 'boolean') {
+    if (obj) {
+      return 'true';
+    } else {
+      return 'false';
+    }
+  }
+
+
+  // Check typeof obj for strings
+  // If true return original string with " "
+
+  // Check if obj is an array
+  // Map obj into a stringArray
+  // return '[' + stringArray.join(,) + ']'
+
+  // Check if typeof is object
+  // declare array = [];
+  // "{ 'key' : 'value'}"
+  // Loop through obj
+    // Create stringified versions of the key value pairs using stringifyJSON
+    // Declare keyString
+    // Declare valueString
+  // array.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]))
+  // return '{' + array,join(,) + '}'
+
+
 };
