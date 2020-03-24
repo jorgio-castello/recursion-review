27aadc64a3bcb31c275cafb0e56e715759b783a3
Jorge Castello <jorgio.castello@gmail.com>
2020-03-23 17:26:07 -0700

    Update test to display actual / expected values

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -8,7 +8,7 @@ describe('parseJSON', function() {
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
-      expect(equality).to.equal(true);
+      expect(result).to.eql(expected);
     });
   });
 
