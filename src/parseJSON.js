f36c394ef353d9efebb14d2945815e5a9dfd00d1
Jorge Castello <jorgio.castello@gmail.com>
2020-03-23 17:26:31 -0700

    Commit current work to have for future reference

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,33 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  //Loop through each character of the json parameter
+  for (let i = 0; i < json.length; i++) {
+    if (json[i] === '[') {
+      let tempStr = json.slice(i + 1);
+      return parseArray(tempStr);
+    }
+  }
+  //if json[i] === '['
+  // parseJSON(']')
 };
+
+let parseArray = function(json) {
+  let result = [];
+  let tempStr = '';
+
+  for (let i = 0; i < json.length; i++) {
+    if (json[i] === ',' || json[i] === '[') {
+      let parsedElement = parseJSON(tempStr);
+      result.push(parsedElement);
+      tempStr = '';
+    }
+    if (json[i] === ']') {
+      return result;
+    }
+
+    tempStr += json[i];
+  }
+};
+
