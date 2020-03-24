43cc2766c3abef9b036a025f995bc5732d9691d5
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-24 00:25:21 -0700

    Implement logic so that function handles nested arrays

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -33,8 +33,45 @@ var parseJSON = function(json) {
       return null;
     }
 
+    //Arrays
+    if (json[i] === '[') {
+      json = json.slice(i + 1);
+      let result = handleArrays(json);
+      return result;
+    }
   }
 };
 
+let handleArrays = function(json) {
+  //declare an arr that will eventually be the final return value of this function
+  let arr = [];
+  //declare a variable tempString
+  let tempString = '';
 
+  //if the first character is a opening bracket for an array, invoke parseJSON
+  if (json[0] === '[') {
+    arr.push(parseJSON(json));
+    return arr;
+  }
+
+  //Loop through the json parameter
+  for (let i = 0; i < json.length; i++) {
+    //if the current character is a comma, parseJSON on the built-up tempString
+    if (json[i] === ',') {
+      arr.push(parseJSON(tempString));
+      tempString = ''; //reset tempString to be empty
+    //if: its a closing bracket
+    } else if (json[i] === ']') {
+      //if tempString has characters in it, invoke parseJSON - arr.push(parsejson(tempString))
+      if (tempString !== '') {
+        arr.push(parseJSON(tempString));
+      }
+      return arr;
+    } else {
+      tempString += json[i]; //concat to the tempString with the current character of json
+    }
+  }
+
+  return arr;
+};
 
