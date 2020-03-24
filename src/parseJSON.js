91353b47679d26c137ac198097d4e428103dc1a4
Jorge Castello <jorgio.castello@gmail.com>
2020-03-23 18:31:39 -0700

    Add logic for numbers, strings, booleans, null

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,36 +1,40 @@
 // this is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
 
+
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
 
   //Loop through each character of the json parameter
   for (let i = 0; i < json.length; i++) {
-    if (json[i] === '[') {
-      let tempStr = json.slice(i + 1);
-      return parseArray(tempStr);
+    //Numbers
+    if ( json.charCodeAt(i) >= 48 && json.charCodeAt(i) <= 57) {
+      return parseInt(json);
     }
+
+    //Strings
+    if (json[i] === '"') {
+      let endIndex = json.indexOf('"', i + 1);
+      return json.slice(i + 1, endIndex);
+    }
+
+    //Booleans: true
+    if ( json === 'true') {
+      return true;
+    }
+
+    if ( json === 'false') {
+      return false;
+    }
+
+    //Null
+    if (json === 'null') {
+      return null;
+    }
+
   }
-  //if json[i] === '['
-  // parseJSON(']')
 };
 
-let parseArray = function(json) {
-  let result = [];
-  let tempStr = '';
 
-  for (let i = 0; i < json.length; i++) {
-    if (json[i] === ',' || json[i] === '[') {
-      let parsedElement = parseJSON(tempStr);
-      result.push(parsedElement);
-      tempStr = '';
-    }
-    if (json[i] === ']') {
-      return result;
-    }
-
-    tempStr += json[i];
-  }
-};
 
